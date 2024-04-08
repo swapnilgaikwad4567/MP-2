87fbcef921f70c29c77d400660794fcb784d28a9
swapnilgaikwad4567 <125345668+swapnilgaikwad4567@users.noreply.github.com>
2024-04-08 09:33:15 +0530

    index.php

--- Swapnil/index.php
+++ Swapnil/index.php
@@ -26,6 +26,17 @@ input::-webkit-inner-spin-button {
 input[type=number] {
   -moz-appearance: textfield;
 }
+.custom-bg{
+  background-color: #2ec1ac;
+}
+.custom-bg:hover{
+  background-color: #279e8c;
+}
+.availability-form{
+  margin-top: -50px;
+  z-index: 2;
+  position: relative;
+}
     
 </style>
 </head>
@@ -191,26 +202,44 @@ input[type=number] {
   <!-- Swiper JS -->
 </div>
 <!--Check Avaibility form-->
-<div class="container">
+<div class="container availability-form">
   <div class="row">
     <div class="col-lg-12 bg-white shadow p-4 rounded">
-      <h5>
+      <h5 class="mb-4">
         Check Booking Avaibility
       </h5>
       <form>
-        <div class="row">
-          <div class="col-lg-3">
+        <div class="row align-items-end">
+          <div class="col-lg-3 mb-3">
           <label class="form-label" style="font-weight: 500;">Check-in</label>
           <input type="date" class="form-control shadow=none">
           </div>
-          <div class="col-lg-3">
+          <div class="col-lg-3 mb-3">
           <label class="form-label" style="font-weight: 500;">Check-out</label>
           <input type="date" class="form-control shadow=none">
           </div>
-          <div class="col-lg-3">
+          <div class="col-lg-3 mb-3">
           <label class="form-label" style="font-weight: 500;">Adult</label>
-
-          </div>          
+          <select class="form-select shadow-none">
+         
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          </select>
+          </div>   
+          
+          <div class="col-lg-2 mb-3">
+          <label class="form-label" style="font-weight: 500;">Children</label>
+          <select class="form-select shadow-none">
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          </select>
+          </div>  
+<div class="col-lg-1 mb-lg-3  mt-2">
+  <button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
+</div>
+        </div>
         </div>
       </form>
     </div>
